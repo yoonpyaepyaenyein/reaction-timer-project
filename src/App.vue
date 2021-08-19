e57c97f1f17049c5970ef9399c121e8bcdd4855a
yoon <yoonpyae1310@gmail.com>
2021-08-19 19:54:21 +0630

    final commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>How fast u can catch me?</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <div v-if="isPlaying">
+    <Block :delay="delay" @endGame="endGame"/>
+  </div>
+    <div v-if="end">
+      <Result :score="score"/>
+    </div>
+  </template>
+
+<script>
+import Block from "./components/Block.vue"
+import Result from "./components/Result.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Result
+  },
+  data(){
+    return{
+      isPlaying:false,
+      delay:null,
+      score:0,
+      end:false
+    }
+  },
+  methods:{
+    start(){
+      this.isPlaying=true;
+      this.delay=2000+Math.random()*5000;
+    },
+  endGame(score){
+   this.score=score;
+   this.isPlaying=false;
+   this.end=true;
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click here{{delay}}</div>
+
+</template>
+
+<script>
+export default {
+  props:['delay'],
+  data(){
+    return{
+      showBlock:false,
+      score:0,
+      timer:null
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.showBlock=true;
+      this.startTimer()
+    },this.delay);
+  },
+  methods:{
+    startTimer(){
+      this.timer=setInterval(()=>{
+        this.score+=50;
+      },50);
+    },
+   stopTimer(){
+    clearInterval(this.timer);
+    this.$emit("endGame" , this.score)
+    }
+  }
+}
+</script>
+<style>
+    .block{
+        background-color: aqua;
+        width: 400px;
+        height: 300px;
+        padding-top:30px ;
+        margin: 20px auto;
+    }
+</style>
